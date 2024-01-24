fe768bfbc76acdb0deae43166d02c1b02da1ab4e
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-01-24 17:18:18 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+
+    const mainElement = document.getElementById('main');
+    mainElement.remove();
+    const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+    newHeader.textContent = 'Sydney is the champion';
+    document.body.appendChild(newHeader);
